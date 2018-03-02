7ac23470c771f973b07c67f656a60ddda82b54e0
Unlogical <sakujo8744@mail.ru>
2018-03-02 23:09:35 +0300

    searching tweets by hardcoded hashtag and showing them on tweets/

--- hashtaganalyzer/urls.py
+++ hashtaganalyzer/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.input, name='input'),
+    url(r'^tweets/$', views.tweets, name='tweets'),
 ]
--- hashtaganalyzer/views.py
+++ hashtaganalyzer/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from . import twitter_functions
 
 # Create your views here.
 def input(request):
     return render(request, 'hashtaganalyzer/input.html', {})
+
+def tweets(request):
+    tweets = twitter_functions.get_tweets()
+    return render(request, 'hashtaganalyzer/tweets.html', {'tweets': tweets})
